aaeb42b16212e92ff1ad065bf0d741254ba8f7c3
Colm Murphy <colmmurphy016@gmail.com>
2024-10-06 13:22:38 +0100

    inject onLandedUpon method instead of setToDirt

--- src/main/java/xyz/colmmurphy/mixin/FarmlandBlockMixin.java
+++ src/main/java/xyz/colmmurphy/mixin/FarmlandBlockMixin.java
@@ -1,6 +1,10 @@
 package xyz.colmmurphy.mixin;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,23 +13,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.slf4j.Logger;
 import xyz.colmmurphy.NoTrampleFarmland;
 
-@Mixin(value=FarmlandBlock.class, priority=1001)
+@Mixin(value = FarmlandBlock.class, priority = 1001)
 public class FarmlandBlockMixin {
     @Unique
     private static final Logger LOGGER = NoTrampleFarmland.LOGGER;
 
     @Inject(
             at = @At("HEAD"),
-            method = "setToDirt",
+            method = "onLandedUpon",
             cancellable = true
     )
-    private static void setToDirt(CallbackInfo callbackInfo) {
-        LOGGER.info("FarmLand Mixin Called");
-        if (callbackInfo.isCancellable() && !callbackInfo.isCancelled()) {
-            LOGGER.info("Cancelling");
-            callbackInfo.cancel();
-        } else {
-            LOGGER.info("Could Not Cancel");
+    private void onLandedUpon(
+            World world,
+            BlockState state,
+            BlockPos pos,
+            Entity entity,
+            float fallDistance,
+            CallbackInfo ci
+    ) {
+        if (ci.isCancellable() && !ci.isCancelled()) {
+            LOGGER.info("Cancelling FarmlandBlock::onLandedUpon");
+            ci.cancel();
+            // fallDistance <= 0 ensures farmland will not be turned to dirt
+            state.getBlock().onLandedUpon(world, state, pos, entity, 0.0f);
         }
     }
 }
