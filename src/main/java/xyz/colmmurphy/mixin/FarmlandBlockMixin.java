bd1ae51efcd81071d167e090e3835a30833f4094
Colm Murphy <colmmurphy016@gmail.com>
2026-08-01 14:53:58 +0100

    Update mod for minecraft 26.2

--- src/main/java/xyz/colmmurphy/mixin/FarmlandBlockMixin.java
+++ src/main/java/xyz/colmmurphy/mixin/FarmlandBlockMixin.java
@@ -1,10 +1,11 @@
 package xyz.colmmurphy.mixin;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.FarmlandBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FarmlandBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.core.BlockPos;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -18,17 +19,18 @@ public abstract class FarmlandBlockMixin extends Block {
     @Unique
     private static final Logger LOGGER = NoTrampleFarmland.LOGGER;
 
-    public FarmlandBlockMixin(Settings settings) {
-        super(settings);
+    public FarmlandBlockMixin(Properties properties) {
+        super(properties);
     }
 
-    @Redirect(method = "onLandedUpon",
+    @Redirect(method = "fallOn",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/block/FarmlandBlock;setToDirt(Lnet/minecraft/entity/Entity;Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V"
+                    target="Lnet/minecraft/world/level/block/FarmlandBlock;turnToDirt(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)V"
             )
     )
-    private static void redirectedSetToDirt(@Nullable Entity entity, BlockState state, World world, BlockPos pos) {
+    private static void turnToDirt(final @Nullable Entity sourceEntity, final BlockState state, final Level level, final BlockPos pos) {
         LOGGER.trace("Redirected call to FarmlandBlock::setToDirt");
     }
+
 }
